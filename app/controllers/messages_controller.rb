6e59b6fa2625a3d346df3edf2abb15b150aeb313
Masayoshi Kawaguchi <k.liverpool0344@gmail.com>
2017-09-26 06:47:49 +0000

    DRY

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,49 @@
+class MessagesController < ApplicationController
+    before_action :set_message, only: [:show, :edit, :update, :destroy]
+    def index
+        @messages=Message.all
+    end
+    def show
+    end
+    def new
+        @message=Message.new
+    end
+    def create
+        @message=Message.new(message_params)
+        
+        if @message.save
+            flash[:success]= "メッセージが正常に投稿されました"
+            redirect_to @message
+        else
+            flash.now[:danger]="メッセージが投稿されませんでした"
+            render :new
+        end
+    end
+    def edit
+    end
+    def update
+        if @message.update(message_params)
+            flash[:success]="メッセージは正常に更新されました"
+            redirect_to @message
+        else
+            flash.now[:danger]="メッセージは更新されませんでした"
+            render :edit
+        end
+    end
+    def destroy
+        @message.destroy
+        
+        flash[:success]="メッセージは正常に削除されました"
+        redirect_to message_url
+    end
+    
+    private
+    
+    def set_message
+        @message=Message.find(params[:id])
+    end
+    
+    def message_params
+        params.require(:message).permit(:content)
+    end
+end
